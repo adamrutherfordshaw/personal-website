f9bbe4f208456d4eb9ac222a44a29280db4b6481
Adam Rutherford-Shaw <adamrutherfordshaw@gmail.com>
2023-05-01 15:10:57 +0100

    fix: add holding page until main content is added

---
+++ src/components/holding-page/holding-page.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HoldingPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 32px;
+  gap: 32px;
+`;
+
+export const Heading = styled.h1``;
+
+export const SubHeading = styled.h2``;
+
+export const WebsiteLoading = styled.p``;
---
+++ src/components/holding-page/holding-page.tsx
@@ -0,0 +1,12 @@
+import * as Styled from "./holding-page.styles";
+
+export const HoldingPage = () => {
+  return (
+    <Styled.HoldingPageContainer>
+      <Styled.Heading>Adam Rutherford-Shaw</Styled.Heading>
+      <Styled.SubHeading>Software Developer</Styled.SubHeading>
+
+      <Styled.WebsiteLoading>Website in progress...</Styled.WebsiteLoading>
+    </Styled.HoldingPageContainer>
+  );
+};
